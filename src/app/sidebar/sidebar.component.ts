8e5bead3f1790bec9940a1e434925650b739441b
Helias Karagozidis <helias.karagozidis@eurodyn.com>
2020-05-03 02:45:21 +0300

    Sidebar Submenus support implemented

--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,42 +1,153 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Router} from '@angular/router';
 
 
 export interface RouteInfo {
-    path: string;
-    title: string;
-    icon: string;
-    class: string;
-    children: RouteInfo[];
+  id: string;
+  path: string;
+  title: string;
+  icon: string;
+  class: string;
+  type: string;
+  children: RouteInfo[];
 }
 
 export const ROUTES: RouteInfo[] = [
-    { path: '/dashboard',     title: 'Dashboard',         icon:'nc-bank',       class: '', children: null },
-    { path: '/icons',         title: 'Icons',             icon:'nc-diamond',    class: '', children: null},
-    { path: '/maps',          title: 'Maps',              icon:'nc-pin-3',      class: '', children: null},
-    { path: '/notifications', title: 'Notifications',     icon:'nc-bell-55',    class: '', children: null },
-    { path: '/user',          title: 'User Profile',      icon:'nc-single-02',  class: '', children: null },
-    { path: '/table',         title: 'Table List',        icon:'nc-tile-56',    class: '', children: null},
-    { path: '/typography',    title: 'Typography',        icon:'nc-caps-small', class: '', children: null},
-    { path: '/table-designer-list', title: 'Table Designer',   icon:'nc-settings', class: '', children: null},
-    { path: '/menu-designer-list', title: 'Menu Designer',   icon:'nc-tile-56', class: '', children: null},
-    // { path: '/upgrade',       title: 'Upgrade to PRO',    icon:'nc-spaceship',  class: 'active-pro' },
-
-
+  // {path: '/dashboard', title: 'Dashboard', icon: 'nc-bank', class: '', children: null},
+  // {path: '#', title: 'Other Menus', icon: 'nc-layout-11', class: 'parent-menu', children: null},
+  // {path: '/table-designer-list', title: 'Table Designer', icon: 'nc-settings', class: '', children: null},
+  // {path: '/menu-designer-list', title: 'Menu Designer', icon: 'nc-tile-56', class: '', children: null},
 ];
 
 @Component({
-    moduleId: module.id,
-    selector: 'sidebar-cmp',
-    templateUrl: 'sidebar.component.html',
+  moduleId: module.id,
+  selector: 'sidebar-cmp',
+  templateUrl: 'sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
 })
 
 export class SidebarComponent implements OnInit {
 
   title = 'appBootstrap';
 
-  public isCollapsed = false;
-    public menuItems: any[];
-    ngOnInit() {
-        this.menuItems = ROUTES.filter(menuItem => menuItem);
+  public menuItems: any[];
+  public selectedMenuItems: any[];
+  public menuHeaders: any[];
+
+  constructor(private router: Router) {
+    // console.log(this.router.url);
+  }
+
+  parentMenuSelection(id: string) {
+    this.menuHeaders = [];
+    this.selectedMenuItems = this.menuItems;
+    this.parentMenuSelectionRecursive(this.selectedMenuItems, id);
+    this.menuHeaders.reverse();
+  }
+
+  parentMenuSelectionRecursive(menuItems: any[], id: string) {
+    for (const menuItem of menuItems) {
+      if (menuItem.id === id) {
+        this.menuHeaders.push(menuItem);
+        this.selectedMenuItems = menuItem.children;
+        return true;
+      } else if (menuItem.type === 'parent-menu') {
+        const foundInBranch = this.parentMenuSelectionRecursive(menuItem.children, id);
+        if (foundInBranch) {
+          this.menuHeaders.push(menuItem);
+          return true;
+        }
+      }
     }
+    return false;
+  }
+
+  parentMenuUnselection(id: string) {
+    console.log('Selected ' + id);
+
+    this.menuHeaders = [];
+    this.selectedMenuItems = this.menuItems;
+    this.parentMenuUnselectionRecursive(this.selectedMenuItems, id);
+    this.menuHeaders.reverse();
+  }
+
+  parentMenuUnselectionRecursive(menuItems: any[], id: string) {
+    for (const menuItem of menuItems) {
+      console.log('Checking ' + menuItem.id);
+      if (menuItem.id === id) {
+        this.selectedMenuItems = menuItems;
+        return true;
+      }
+      if (menuItem.type === 'parent-menu') {
+        const foundInBranch = this.parentMenuUnselectionRecursive(menuItem.children, id);
+        if (foundInBranch) {
+          this.menuHeaders.push(menuItem);
+          return true;
+        }
+      }
+    }
+    return false;
+  }
+
+  ngOnInit() {
+    this.menuItems = [];
+
+    this.menuItems.push({id: '1', path: '/dashboard', title: 'Dashboard', icon: 'nc-bank', type: 'link', class: '', children: null});
+    this.menuItems.push({
+      id: '2',
+      path: '#',
+      title: 'Other Menus',
+      icon: 'nc-layout-11',
+      type: 'parent-menu',
+      class: 'parent-menu',
+      children: null
+    });
+    this.menuItems.push({
+      id: '3',
+      path: '/table-designer-list',
+      title: 'Table Designer',
+      icon: 'nc-settings',
+      type: 'link',
+      class: '',
+      children: null
+    });
+    this.menuItems.push({
+      id: '4',
+      path: '/menu-designer-list',
+      title: 'Menu Designer',
+      icon: 'nc-tile-56',
+      type: 'link',
+      class: '',
+      children: null
+    });
+
+    this.menuItems[1].children =
+      [
+        {id: '11', path: '#', title: 'Second submenu', icon: 'nc-caps-small', type: 'parent-menu', class: 'parent-menu', children: null},
+        {id: '5', path: '/icons', title: 'Icons', icon: 'nc-diamond', type: 'link', class: '', children: null},
+        {id: '6', path: '/maps', title: 'Maps', icon: 'nc-pin-3', type: 'link', class: '', children: null},
+        {id: '7', path: '/notifications', title: 'Notifications', icon: 'nc-bell-55', type: 'link', class: '', children: null},
+        {id: '8', path: '/user', title: 'User Profile', icon: 'nc-single-02', type: 'link', class: '', children: null},
+        {id: '9', path: '/table', title: 'Table List', icon: 'nc-tile-56', type: 'link', class: '', children: null},
+        {id: '10', path: '/typography', title: 'Typography', icon: 'nc-caps-small', type: 'link', class: '', children: null},
+      ];
+
+    this.menuItems[1].children[0].children =
+      [
+        {id: '12', path: '#', title: 'third', icon: 'nc-caps-small', type: 'parent-menu', class: 'parent-menu', children: null},
+        {id: '13', path: '/icons', title: 'Icons', icon: 'nc-diamond', type: 'link', class: '', children: null},
+        {id: '16', path: '/maps', title: 'Maps', icon: 'nc-pin-3', type: 'link', class: '', children: null},
+        {id: '17', path: '/notifications', title: 'Notifications', icon: 'nc-bell-55', type: 'link', class: '', children: null},
+        {id: '18', path: '/user', title: 'User Profile', icon: 'nc-single-02', type: 'link', class: '', children: null},
+        {id: '19', path: '/table', title: 'Table List', icon: 'nc-tile-56', type: 'link', class: '', children: null},
+        {id: '20', path: '/typography', title: 'Typography', icon: 'nc-caps-small', type: 'link', class: '', children: null},
+      ];
+    this.menuItems[1].children[0].children[0].children = [
+      {id: '21', path: '/typography', title: 'Typography', icon: 'nc-caps-small', type: 'link', class: '', children: null},
+    ];
+
+    this.selectedMenuItems = this.menuItems;
+
+
+  }
 }
