b888fde994a54b91cf779a749b5e6dbdef3e27aa
christos.aslamatzidis <christos.aslamatzidis@eurodyn.com>
2022-10-31 13:48:36 +0200

    Delete pages irrelevant to simple user

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,21 +13,9 @@ import {AppComponent} from './app.component';
 import {AppRoutes} from './app.routing';
 
 import {AdminLayoutComponent} from './layouts/admin-layout/admin-layout.component';
-import {TableDesignerFormComponent} from './pages/table-designer/table-designer-form/table-designer-form.component';
-import {TableDesignerListComponent} from './pages/table-designer/table-designer-list/table-designer-list.component';
 import {FormsModule} from '@angular/forms';
-import {MenuDesignerListComponent} from './pages/menu-designer/menu-designer-list/menu-designer-list.component';
-import {MenuDesignerFormComponent} from './pages/menu-designer/menu-designer-form/menu-designer-form.component';
 import {LoginComponent} from './pages/login/login.component';
-import {ListDesignerFormComponent} from './pages/list-designer/list-designer-form/list-designer-form.component';
-import {ListDesignerListComponent} from './pages/list-designer/list-designer-list/list-designer-list.component';
-import {ComponentDesignerListComponent} from './pages/component-designer/component-designer-list/component-designer-list.component';
-import {ComponentDesignerFormComponent} from './pages/component-designer/component-designer-form/component-designer-form.component';
-import {ViewDesignerListComponent} from './pages/view-designer/view-designer-list/view-designer-list.component';
-import {ViewDesignerFormComponent} from './pages/view-designer/view-designer-form/view-designer-form.component';
 import {ListComponent} from './pages/list/list/list.component';
-import {AppViewDesignerFormComponent} from './pages/appview-designer/app-view-designer-form/app-view-designer-form.component';
-import {AppViewDesignerListComponent} from './pages/appview-designer/app-view-designer-list/app-view-designer-list.component';
 import {NgbDateAdapter, NgbDateParserFormatter, NgbDatepickerModule, NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import {DatePickerComponent} from './form-controlls/date-picker/date-picker.component';
 import {NgbDateFRParserFormatter} from './form-controlls/date-picker/ngb-date-frparser-formatter';
@@ -35,181 +23,79 @@ import {NgbUTCStringAdapter} from './form-controlls/date-picker/ngb-utcstring-ad
 import {SofiaDateDirective} from './directives/sofia-date.directive';
 import {NgxMaskModule} from 'ngx-mask';
 import {DatePipe} from '@angular/common';
-import {UserFormComponent} from './pages/user/user-form/user-form.component';
-import {UserListComponent} from './pages/user/user-list/user-list.component';
 import {AuthenticationHeaderInterceptor} from './interceptors/authentication-header-interceptor';
 import {HttpRequestLoadingInterceptor} from './interceptors/http-request-loading.interceptor';
 import {HttpRequestErrorInterceptor} from './interceptors/http-request-error.interceptor';
 import {ListSelectorComponent} from './form-controlls/list-selector/list-selector.component';
-import {FormDesignerFormComponent} from './pages/form-designer/form-designer-form/form-designer-form.component';
-import {FormDesignerListComponent} from './pages/form-designer/form-designer-list/form-designer-list.component';
-import {FormDesignerFormToolBoxComponent} from './pages/form-designer/form-designer-form-tool-box/form-designer-form-tool-box.component';
 import {FormComponent} from './pages/form/form/form.component';
 import {NumericInputComponent} from './form-controlls/numeric-input/numeric-input.component';
 import {VarcharInputComponent} from './form-controlls/varchar-input/varchar-input.component';
 import {TextInputComponent} from './form-controlls/text-input/text-input.component';
 import {FormTableComponent} from './pages/form/form/form-table/form-table.component';
-import {ChartDesignerFormComponent} from './pages/chart-designer/chart-designer-form/chart-designer-form.component';
-import {ChartDesignerListComponent} from './pages/chart-designer/chart-designer-list/chart-designer-list.component';
-import {InfoCardDesignerFormComponent} from './pages/info-card-designer/info-card-designer-form/info-card-designer-form.component';
-import {InfoCardDesignerListComponent} from './pages/info-card-designer/info-card-designer-list/info-card-designer-list.component';
 import {DashboardComponent} from './pages/dashboard/dashboard/dashboard.component';
-import {DashboardDesignerFormComponent} from './pages/dashboard-designer/dashboard-designer-form/dashboard-designer-form.component';
-import {DashboardDesignerListComponent} from './pages/dashboard-designer/dashboard-designer-list/dashboard-designer-list.component';
 import {ChartComponent} from './pages/chart/chart.component';
 import {InfoCardComponent} from './pages/info-card/info-card.component';
 import {EmptyComponent} from './pages/empty/empty.component';
 import {YesNoDialogComponent} from './shared/yes-no-dialog/yes-no-dialog.component';
-import {FlowboardDesignerFormComponent} from './pages/flowboard-designer/flowboard-designer-form/flowboard-designer-form.component';
-import {FlowboardDesignerListComponent} from './pages/flowboard-designer/flowboard-designer-list/flowboard-designer-list.component';
-import {ReportDesignerListComponent} from './pages/report-designer/report-designer-list/report-designer-list.component';
-import {ReportDesignerFormComponent} from './pages/report-designer/report-designer-form/report-designer-form.component';
 import {OkDialogComponent} from './shared/ok-dialog/ok-dialog.component';
-import {XlsImportDesignerFormComponent} from './pages/xms-import-designer/xls-import-designer-form/xls-import-designer-form.component';
-import {XlsImportDesignerListComponent} from './pages/xms-import-designer/xls-import-designer-list/xls-import-designer-list.component';
 import {XlsImportComponent} from './pages/xls-import/xls-import.component';
-import { SearchDesignerFormComponent } from './pages/search-designer/search-designer-form/search-designer-form.component';
-import { SearchDesignerListComponent } from './pages/search-designer/search-designer-list/search-designer-list.component';
 import {Title} from '@angular/platform-browser';
 import { ComboBoxComponent } from './form-controlls/combo-box/combo-box.component';
 import { SearchComponent } from './pages/search/search/search.component';
-
-import { UserGroupListComponent } from './pages/usergroup/user-group-list/user-group-list.component';
-import { UserGroupFormComponent } from './pages/usergroup/user-group-form/user-group-form.component';
-
-import { CustomQueryFormComponent } from './pages/custom-query/custom-query-form/custom-query-form.component';
-import { CustomQueryListComponent } from './pages/custom-query/custom-query-list/custom-query-list.component';
-import { SecurityListComponent } from './pages/security/security-list/security-list.component';
-import { SecurityFormComponent } from './pages/security/security-form/security-form.component';
-
 import { ListWrapperComponent } from './pages/list/list-wrapper/list-wrapper.component';
 import { AutocompleteComboBoxComponent } from './form-controlls/autocomplete-combo-box/autocomplete-combo-box.component';
 import { CheckBoxComponent } from './form-controlls/check-box/check-box.component';
 import { CallBackComponent } from './pages/call-back/call-back.component';
 import { FormWrapperComponent } from './pages/form/form-wrapper/form-wrapper.component';
-import { HtmlDashboardDesignerFormComponent } from './pages/html-dashboard-designer/html-dashboard-designer-form/html-dashboard-designer-form.component';
-import { HtmlDashboardDesignerListComponent } from './pages/html-dashboard-designer/html-dashboard-designer-list/html-dashboard-designer-list.component';
 import { HtmlDashboardComponent } from './pages/html-dashboard/html-dashboard/html-dashboard.component';
 import { SafeHtmlPipe } from './services/system/safe-html.pipe';
-import { ExpressionViewerComponent } from './pages/expression-viewer/expression-viewer.component';
-import { RoleListComponent } from './pages/role/role-list/role-list.component';
-import { RoleFormComponent } from './pages/role/role-form/role-form.component';
-import { LanguageDesignerListComponent } from './pages/language-designer/language-designer-list/language-designer-list.component';
-import { LanguageDesignerFormComponent } from './pages/language-designer/language-designer-form/language-designer-form.component';
-import { ListDesignerTranslationFormComponent } from './pages/list-designer/list-designer-translation-form/list-designer-translation-form.component';
 import {AceModule} from 'ngx-ace-wrapper';
 import { ChipsComponent } from './form-controlls/chips/chips.component';
 import { PivotListComponent } from './pages/pivot-list/pivot-list/pivot-list.component';
 import { PivotListWrapperComponent } from './pages/pivot-list/pivot-list-wrapper/pivot-list-wrapper.component';
 import { DashboardWrapperComponent } from './pages/dashboard/dashboard-wrapper/dashboard-wrapper.component';
 import {ChartsModule} from 'ng2-charts';
-import { MenuDesignerTranslationFormComponent } from './pages/menu-designer/menu-designer-translation-form/menu-designer-translation-form.component';
-import { DataTransferListComponent } from './pages/data_transfer/data-transfer-list/data-transfer-list.component';
-import { DataTransferFormComponent } from './pages/data_transfer/data-transfer-form/data-transfer-form.component';
-import { AuthorDashboardComponent } from './pages/author-dashboard/author-dashboard.component';
-import { SettingsComponent } from './pages/settings/settings.component';
 import { PasswordInputComponent } from './form-controlls/password-input/password-input.component';
 import { FileSelectorComponent } from './form-controlls/file-selector/file-selector.component';
 import { TimelineComponent } from './pages/timeline/timeline/timeline.component';
-import { TimelineDesignerListComponent } from './pages/timeline-designer/timeline-designer-list/timeline-designer-list.component';
-import { TimelineDesignerFormComponent } from './pages/timeline-designer/timeline-designer-form/timeline-designer-form.component';
-import { SseNotificationTemplateListComponent } from './pages/sse-notification-template/sse-notification-template-list/sse-notification-template-list.component';
-import { SseNotificationTemplateFormComponent } from './pages/sse-notification-template/sse-notification-template-form/sse-notification-template-form.component';
-import { BusinessUnitDesignerFormComponent } from './pages/business-unit-designer/business-unit-designer-form/business-unit-designer-form.component';
-import { BusinessUnitDesignerListComponent } from './pages/business-unit-designer/business-unit-designer-list/business-unit-designer-list.component';
+
 @NgModule({
   declarations: [
     AppComponent,
     AdminLayoutComponent,
-    TableDesignerFormComponent,
-    TableDesignerListComponent,
-    MenuDesignerListComponent,
-    MenuDesignerFormComponent,
     LoginComponent,
-    ListDesignerFormComponent,
-    ListDesignerListComponent,
-    ComponentDesignerListComponent,
-    ComponentDesignerFormComponent,
-    ViewDesignerListComponent,
-    ViewDesignerFormComponent,
     ListComponent,
-    AppViewDesignerFormComponent,
-    AppViewDesignerListComponent,
     DatePickerComponent,
     SofiaDateDirective,
-    UserFormComponent,
-    UserListComponent,
     ListSelectorComponent,
-    FormDesignerFormComponent,
-    FormDesignerListComponent,
-    FormDesignerFormToolBoxComponent,
     FormComponent,
     NumericInputComponent,
     VarcharInputComponent,
     TextInputComponent,
     FormTableComponent,
-    ChartDesignerFormComponent,
-    ChartDesignerListComponent,
-    InfoCardDesignerFormComponent,
-    InfoCardDesignerListComponent,
     DashboardComponent,
-    DashboardDesignerFormComponent,
-    DashboardDesignerListComponent,
     ChartComponent,
     InfoCardComponent,
     EmptyComponent,
     YesNoDialogComponent,
-    FlowboardDesignerFormComponent,
-    FlowboardDesignerListComponent,
-    ReportDesignerListComponent,
-    ReportDesignerFormComponent,
     OkDialogComponent,
-    XlsImportDesignerFormComponent,
-    XlsImportDesignerListComponent,
     XlsImportComponent,
-    SearchDesignerFormComponent,
-    SearchDesignerListComponent,
     ComboBoxComponent,
     SearchComponent,
-    UserGroupListComponent,
-    UserGroupFormComponent,
-    CustomQueryFormComponent,
-    CustomQueryListComponent,
-    SecurityListComponent,
-    SecurityFormComponent,
     ListWrapperComponent,
     AutocompleteComboBoxComponent,
     CheckBoxComponent,
     CallBackComponent,
     FormWrapperComponent,
-    HtmlDashboardDesignerFormComponent,
-    HtmlDashboardDesignerListComponent,
     HtmlDashboardComponent,
     SafeHtmlPipe,
-    ExpressionViewerComponent,
-    RoleListComponent,
-    RoleFormComponent,
-    LanguageDesignerListComponent,
-    LanguageDesignerFormComponent,
-    ListDesignerTranslationFormComponent,
     ChipsComponent,
     PivotListComponent,
     PivotListWrapperComponent,
     DashboardWrapperComponent,
-    MenuDesignerTranslationFormComponent,
-    DataTransferListComponent,
-    DataTransferFormComponent,
-    AuthorDashboardComponent,
-    SettingsComponent,
     PasswordInputComponent,
     FileSelectorComponent,
     TimelineComponent,
-    TimelineDesignerListComponent,
-    TimelineDesignerFormComponent,
-    SseNotificationTemplateListComponent,
-    SseNotificationTemplateFormComponent,
-    BusinessUnitDesignerFormComponent,
-    BusinessUnitDesignerListComponent
   ],
   imports: [
     BrowserAnimationsModule,
--- src/app/layouts/admin-layout/admin-layout.routing.ts
+++ src/app/layouts/admin-layout/admin-layout.routing.ts
@@ -2,140 +2,36 @@ import {Routes} from '@angular/router';
 
 import {UserComponent} from '../../pages/user/user.component';
 import {TableComponent} from '../../pages/table/table.component';
-import {TableDesignerListComponent} from '../../pages/table-designer/table-designer-list/table-designer-list.component';
-import {TableDesignerFormComponent} from '../../pages/table-designer/table-designer-form/table-designer-form.component';
-import {MenuDesignerListComponent} from '../../pages/menu-designer/menu-designer-list/menu-designer-list.component';
-import {MenuDesignerFormComponent} from '../../pages/menu-designer/menu-designer-form/menu-designer-form.component';
-import {ListDesignerListComponent} from '../../pages/list-designer/list-designer-list/list-designer-list.component';
-import {ListDesignerFormComponent} from '../../pages/list-designer/list-designer-form/list-designer-form.component';
-import {ComponentDesignerFormComponent} from '../../pages/component-designer/component-designer-form/component-designer-form.component';
-import {ComponentDesignerListComponent} from '../../pages/component-designer/component-designer-list/component-designer-list.component';
-import {ViewDesignerListComponent} from '../../pages/view-designer/view-designer-list/view-designer-list.component';
-import {ViewDesignerFormComponent} from '../../pages/view-designer/view-designer-form/view-designer-form.component';
 import {ListComponent} from '../../pages/list/list/list.component';
 import {CommandNavigatorService} from '../../services/system/command-navigator.service';
-import {AppViewDesignerListComponent} from '../../pages/appview-designer/app-view-designer-list/app-view-designer-list.component';
-import {AppViewDesignerFormComponent} from '../../pages/appview-designer/app-view-designer-form/app-view-designer-form.component';
-import {UserFormComponent} from '../../pages/user/user-form/user-form.component';
-import {UserListComponent} from '../../pages/user/user-list/user-list.component';
-import {FormDesignerListComponent} from '../../pages/form-designer/form-designer-list/form-designer-list.component';
-import {FormDesignerFormComponent} from '../../pages/form-designer/form-designer-form/form-designer-form.component';
 import {FormComponent} from '../../pages/form/form/form.component';
-import {ChartDesignerFormComponent} from '../../pages/chart-designer/chart-designer-form/chart-designer-form.component';
-import {ChartDesignerListComponent} from '../../pages/chart-designer/chart-designer-list/chart-designer-list.component';
-import {InfoCardDesignerFormComponent} from '../../pages/info-card-designer/info-card-designer-form/info-card-designer-form.component';
-import {InfoCardDesignerListComponent} from '../../pages/info-card-designer/info-card-designer-list/info-card-designer-list.component';
-import {DashboardDesignerFormComponent} from '../../pages/dashboard-designer/dashboard-designer-form/dashboard-designer-form.component';
-import {DashboardDesignerListComponent} from '../../pages/dashboard-designer/dashboard-designer-list/dashboard-designer-list.component';
 import {EmptyComponent} from '../../pages/empty/empty.component';
 import {DashboardComponent} from '../../pages/dashboard/dashboard/dashboard.component';
-import {ReportDesignerFormComponent} from '../../pages/report-designer/report-designer-form/report-designer-form.component';
-import {ReportDesignerListComponent} from '../../pages/report-designer/report-designer-list/report-designer-list.component';
-import {XlsImportDesignerFormComponent} from '../../pages/xms-import-designer/xls-import-designer-form/xls-import-designer-form.component';
-import {XlsImportDesignerListComponent} from '../../pages/xms-import-designer/xls-import-designer-list/xls-import-designer-list.component';
 import {XlsImportComponent} from '../../pages/xls-import/xls-import.component';
-import {SearchDesignerListComponent} from '../../pages/search-designer/search-designer-list/search-designer-list.component';
-import {SearchDesignerFormComponent} from '../../pages/search-designer/search-designer-form/search-designer-form.component';
 import {SearchComponent} from '../../pages/search/search/search.component';
-import {UserGroupListComponent} from '../../pages/usergroup/user-group-list/user-group-list.component';
-import {UserGroupFormComponent} from '../../pages/usergroup/user-group-form/user-group-form.component';
-import { CustomQueryListComponent } from 'app/pages/custom-query/custom-query-list/custom-query-list.component';
-import { CustomQueryFormComponent } from 'app/pages/custom-query/custom-query-form/custom-query-form.component';
-import { SecurityListComponent } from 'app/pages/security/security-list/security-list.component';
 import {ListWrapperComponent} from '../../pages/list/list-wrapper/list-wrapper.component';
 import {AuthGuard} from '../../guards/auth.guard';
 import {FormWrapperComponent} from '../../pages/form/form-wrapper/form-wrapper.component';
-import {HtmlDashboardDesignerFormComponent} from '../../pages/html-dashboard-designer/html-dashboard-designer-form/html-dashboard-designer-form.component';
-import {HtmlDashboardDesignerListComponent} from '../../pages/html-dashboard-designer/html-dashboard-designer-list/html-dashboard-designer-list.component';
-import {ExpressionViewerComponent} from '../../pages/expression-viewer/expression-viewer.component';
-import {RoleListComponent} from '../../pages/role/role-list/role-list.component';
-import {RoleFormComponent} from '../../pages/role/role-form/role-form.component';
-import {LanguageDesignerFormComponent} from '../../pages/language-designer/language-designer-form/language-designer-form.component';
-import {LanguageDesignerListComponent} from '../../pages/language-designer/language-designer-list/language-designer-list.component';
-import {ListDesignerTranslationFormComponent} from '../../pages/list-designer/list-designer-translation-form/list-designer-translation-form.component';
 import {PivotListComponent} from '../../pages/pivot-list/pivot-list/pivot-list.component';
 import {PivotListWrapperComponent} from '../../pages/pivot-list/pivot-list-wrapper/pivot-list-wrapper.component';
-import {MenuDesignerTranslationFormComponent} from '../../pages/menu-designer/menu-designer-translation-form/menu-designer-translation-form.component';
-import {DataTransferFormComponent} from '../../pages/data_transfer/data-transfer-form/data-transfer-form.component';
-import {DataTransferListComponent} from '../../pages/data_transfer/data-transfer-list/data-transfer-list.component';
-import {AuthorDashboardComponent} from '../../pages/author-dashboard/author-dashboard.component';
-import {SettingsComponent} from '../../pages/settings/settings.component';
 import {TimelineComponent} from '../../pages/timeline/timeline/timeline.component';
-import {TimelineDesignerListComponent} from '../../pages/timeline-designer/timeline-designer-list/timeline-designer-list.component';
-import {TimelineDesignerFormComponent} from '../../pages/timeline-designer/timeline-designer-form/timeline-designer-form.component';
-import {SseNotificationTemplateListComponent} from '../../pages/sse-notification-template/sse-notification-template-list/sse-notification-template-list.component';
-import {SseNotificationTemplateFormComponent} from '../../pages/sse-notification-template/sse-notification-template-form/sse-notification-template-form.component';
-import {BusinessUnitDesignerFormComponent} from '../../pages/business-unit-designer/business-unit-designer-form/business-unit-designer-form.component';
-import {BusinessUnitDesignerListComponent} from '../../pages/business-unit-designer/business-unit-designer-list/business-unit-designer-list.component';
 
 export const AdminLayoutRoutes: Routes = [
   {path: 'user', component: UserComponent, canActivate: [AuthGuard]},
   {path: 'user', component: UserComponent, canActivate: [AuthGuard]},
   {path: 'table', component: TableComponent, canActivate: [AuthGuard]},
-  {path: 'table-designer-list', component: TableDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'table-designer-form', component: TableDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'menu-designer-list', component: MenuDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'menu-designer-form', component: MenuDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'menu-designer-translation-form', component: MenuDesignerTranslationFormComponent, canActivate: [AuthGuard]},
-  {path: 'list-designer-list', component: ListDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'list-designer-form', component: ListDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'list-designer-translation-form', component: ListDesignerTranslationFormComponent, canActivate: [AuthGuard]},
-  {path: 'component-designer-form', component: ComponentDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'component-designer-list', component: ComponentDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'view-designer-list', component: ViewDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'view-designer-form', component: ViewDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'appview-designer-list', component: AppViewDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'appview-designer-form', component: AppViewDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'user-form', component: UserFormComponent, canActivate: [AuthGuard]},
-  {path: 'user-list', component: UserListComponent, canActivate: [AuthGuard]},
   {path: 'list', component: ListComponent, canActivate: [AuthGuard]},
   {path: 'list-alt', component: ListWrapperComponent, canActivate: [AuthGuard]},
   {path: 'pivotlist', component: PivotListComponent, canActivate: [AuthGuard]},
   {path: 'pivotlist-alt', component: PivotListWrapperComponent, canActivate: [AuthGuard]},
-  {path: 'form-designer-list', component: FormDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'form-designer-form', component: FormDesignerFormComponent, canActivate: [AuthGuard]},
   {path: 'form', component: FormComponent, canActivate: [AuthGuard]},
   {path: 'form-alt', component: FormWrapperComponent, canActivate: [AuthGuard]},
-  {path: 'chart-designer-form', component: ChartDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'chart-designer-list', component: ChartDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'info-card-designer-form', component: InfoCardDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'info-card-designer-list', component: InfoCardDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'dashboard-designer-form', component: DashboardDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'dashboard-designer-list', component: DashboardDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'report-designer-list', component: ReportDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'report-designer-form', component: ReportDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'xls-import-designer-form', component: XlsImportDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'xls-import-designer-list', component: XlsImportDesignerListComponent, canActivate: [AuthGuard]},
   {path: 'xls-import', component: XlsImportComponent, canActivate: [AuthGuard]},
   {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]},
   {path: 'dashboard-alt', component: DashboardComponent, canActivate: [AuthGuard]},
   {path: 'default', component: EmptyComponent, canActivate: [AuthGuard]},
-  {path: 'search-designer-list', component: SearchDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'search-designer-form', component: SearchDesignerFormComponent, canActivate: [AuthGuard]},
   {path: 'search', component: SearchComponent, canActivate: [AuthGuard]},
-  {path: 'user-group-list', component: UserGroupListComponent, canActivate: [AuthGuard]},
-  {path: 'user-group-form', component: UserGroupFormComponent, canActivate: [AuthGuard]},
-  {path: 'custom-query-form', component: CustomQueryFormComponent, canActivate: [AuthGuard] },
-  {path: 'custom-query-list', component: CustomQueryListComponent, canActivate: [AuthGuard]},
-  {path: 'security-list', component: SecurityListComponent, canActivate: [AuthGuard]},
-  {path: 'html-dashboard-designer-form', component: HtmlDashboardDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'html-dashboard-designer-list', component: HtmlDashboardDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'expression-viewer', component: ExpressionViewerComponent, canActivate: [AuthGuard]},
-  {path: 'role-list', component: RoleListComponent, canActivate: [AuthGuard]},
-  {path: 'role-form', component: RoleFormComponent, canActivate: [AuthGuard]},
-  {path: 'language-designer-form', component: LanguageDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'language-designer-list', component: LanguageDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'data-transfer-form', component: DataTransferFormComponent, canActivate: [AuthGuard]},
-  {path: 'data-transfer-list', component: DataTransferListComponent, canActivate: [AuthGuard]},
-  {path: 'author-dashboard', component: AuthorDashboardComponent, canActivate: [AuthGuard]},
-  {path: 'settings', component: SettingsComponent, canActivate: [AuthGuard]},
   {path: 'timeline', component: TimelineComponent, canActivate: [AuthGuard]},
-  {path: 'timeline-designer-list', component: TimelineDesignerListComponent, canActivate: [AuthGuard]},
-  {path: 'timeline-designer-form', component: TimelineDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'notification-template-list', component: SseNotificationTemplateListComponent, canActivate: [AuthGuard]},
-  {path: 'notification-template-form', component: SseNotificationTemplateFormComponent, canActivate: [AuthGuard]},
-  {path: 'business-unit-designer-form', component: BusinessUnitDesignerFormComponent, canActivate: [AuthGuard]},
-  {path: 'business-unit-designer-list', component: BusinessUnitDesignerListComponent, canActivate: [AuthGuard]},
 ];
 
 CommandNavigatorService.NavPages = AdminLayoutRoutes;
