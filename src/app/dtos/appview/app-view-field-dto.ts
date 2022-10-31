5260dc781fabb8bb4954b2537fe08ea954b8662a
christos.aslamatzidis <christos.aslamatzidis@eurodyn.com>
2022-10-31 15:23:10 +0200

    remove unnecessary dto

--- src/app/dtos/appview/app-view-field-dto.ts
+++
@@ -1,7 +0,0 @@
-import {PersistEntityFieldDTO} from '../persistEntity/persist-entity-field-dto';
-
-export class AppViewFieldDTO extends PersistEntityFieldDTO {
-
-  public entitytype = 'AppViewField';
-
-}
--- src/app/dtos/view/view-field-dto.ts
+++
@@ -1,7 +0,0 @@
-import {PersistEntityFieldDTO} from '../persistEntity/persist-entity-field-dto';
-
-export class ViewFieldDTO extends PersistEntityFieldDTO {
-
-  private entitytype = 'ViewField';
-
-}
