0cf0eb310c25c50ccfeb098628edc1be825b2ef5
HeliasEurodyn <helias.karagozidis@eurodyn.com>
2022-11-07 18:30:15 +0200

    Combobox updated and added to List Filters

--- src/app/form-controlls/combo-box/combo-box.component.ts
+++ src/app/form-controlls/combo-box/combo-box.component.ts
@@ -14,20 +14,37 @@ export class ComboBoxComponent implements OnInit {
   @Output() keydownEvent = new EventEmitter<KeyboardEvent>();
   @Output() focusEvent = new EventEmitter<FocusEvent>();
   @Output() eventOccured = new EventEmitter<any>();
-  comboOptions: any;
+  @Input() value: string;
+  @Output() valueChange = new EventEmitter<string>();
+  @Input() comboOptions: any = null;
+  @Input() defaultValue: string = null;
 
   constructor() {
   }
 
   ngOnInit(): void {
-    this.comboOptions = JSON.parse(this.componentPersistEntityFieldDTO.assignment.editor);
+
+    if (this.componentPersistEntityFieldDTO != null) {
+      this.comboOptions = JSON.parse(this.componentPersistEntityFieldDTO.assignment.editor);
+    } else if (this.comboOptions != null) {
+      this.comboOptions = JSON.parse(this.comboOptions);
+    }
+
+    if (!this.defaultValue == null) {
+      this.value = this.defaultValue;
+    }
+
+    if (this.componentPersistEntityFieldDTO != null) {
+      this.value = this.componentPersistEntityFieldDTO.value;
+    }
+
   }
 
   eventOccuredActions(eventtype: string, event: any) {
     this.eventOccured.emit(
       {
-        entityCode: this.componentPersistEntityDTO.code,
-        fieldCode: this.componentPersistEntityFieldDTO.code,
+        entityCode: (this.componentPersistEntityDTO == null ? this.value : this.componentPersistEntityDTO.code),
+        fieldCode: (this.componentPersistEntityFieldDTO == null ? this.value : this.componentPersistEntityFieldDTO.code),
         eventtype: eventtype,
         event: event
       }
@@ -46,12 +63,19 @@ export class ComboBoxComponent implements OnInit {
     this.componentPersistEntityFieldDTO.value = null;
   }
 
-  selectItem($event: any) {
-    if ($event === 'null' || $event === 'undefined') {
-      this.componentPersistEntityFieldDTO.value = null;
-    } else {
-      this.componentPersistEntityFieldDTO.value = $event;
+  selectItem(value: any) {
+
+    if (value === 'null' || value === 'undefined') {
+      value = null;
     }
+    this.valueChange.emit(value);
+
+    if (this.componentPersistEntityFieldDTO == null) {
+      return;
+    }
+
+    this.componentPersistEntityFieldDTO.value = value;
+
   }
 
   focusInputField(input) {
