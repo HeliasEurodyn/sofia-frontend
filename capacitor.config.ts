a5246735a5f6a7907dd671c28c97aac87b3629f7
HeliasEurodyn <helias.karagozidis@eurodyn.com>
2023-01-02 14:03:52 +0200

    New Android background change & Configuration

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.ed.onenet',
-  appName: ' onenet',
+  appName: 'onenet',
   webDir: 'dist',
   bundledWebRuntime: false
 };
