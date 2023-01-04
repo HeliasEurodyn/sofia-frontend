c79b1e6c3b394e2bdb32d24304d3a68b39706601
HeliasEurodyn <helias.karagozidis@eurodyn.com>
2023-01-04 18:39:09 +0200

    keycloak redirect url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   serverUrl: window['backend_url'] || 'http://localhost:15602/api',
   serverOnProxyPath: window['backend_on_proxy_path'] || '',
-  frontendUrl: 'http://localhost:5020'
+  frontendUrl: 'https://onenet-ngsi-ld.eurodyn.com'
 };
