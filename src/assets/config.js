b83d36cd34b3ddf53bdaeb25ba60b17eb785de12
helias <helias.karagozidis@eurodyn.com>
2023-03-06 12:18:51 +0200

    Keycloak login enable/disable functionality

--- src/assets/config.js
+++ src/assets/config.js
@@ -1,2 +1,3 @@
 window['backend_url'] = '';
 window['backend_on_proxy_path'] = '/api';
+window['keycloak_login'] = 'no';
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   serverUrl: window['backend_url'] || 'http://localhost:15602/api',
-  serverOnProxyPath: window['backend_on_proxy_path'] || ''
+  serverOnProxyPath: window['backend_on_proxy_path'] || '',
+  keycloakLogin :  window['keycloak_login']  || 'no'
 };
