cf6870e22506da54a419819c91d45fba3cb83273
helias <helias.karagozidis@eurodyn.com>
2023-04-20 13:40:07 +0300

    Rules Implementations

---
+++ src/app/dtos/rule/rule-settings-dto.spec.ts
@@ -0,0 +1,7 @@
+import { RuleSettingsDTO } from './rule-settings-dto';
+
+describe('RuleSettingsDTO', () => {
+  it('should create an instance', () => {
+    expect(new RuleSettingsDTO()).toBeTruthy();
+  });
+});
